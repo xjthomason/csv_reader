f2de8a39d5728b9a6c5cc35eecb417959d7b4490
root <root@localhost.localdomain>
2018-03-28 11:52:56 -0400

    Got All Users

--- api_query_users.py
+++ api_query_users.py
@@ -19,47 +19,55 @@ def getUsers():
 	credentials = creds.get_credentials_users()
 	http = credentials.authorize(httplib2.Http())
 	service = discovery.build('admin', 'directory_v1', http=http)
+	cont = '' #this will become nextPageToken
+	#errors = 0
+
 
 	print('Retrieving Google Admin User Account Information...')
-	results = service.users().list(domain = 'avx.com', maxResults = 500, orderBy = 'email').execute()
+	print('Users:')
 	
-	#while results.get('nextPageToken'):
-	#	users = results.get('users', [])
+	while True:
+		
+		results = service.users().list(domain = 'avx.com', maxResults = 500, orderBy = 'email', pageToken = cont).execute()
+		users = results.get('users', [])
 
-	users = results.get('users', [])
-
-	try:
-		if not users:
-			print('Nothing')
-		else:
-			print('Users:')
-			for user in users:
-				#if user['creationTime'] >= week_ago:
-				print (u'{0}, {1}, {2}, {3}, {4}'.format(user['primaryEmail'],user['name']['givenName'],user['name']['familyName'],user['name']['fullName'],user['orgUnitPath']))
-				#else:
-				#	continue
-	except Exception, e:
-		pass
+		try:
 			
-	cont = (results.get('nextPageToken'))
+			if not users:
+				print('Nothing')
+			else:
+				for user in users:
+					#if user['creationTime'] >= week_ago:
+					print (u'{0}, {1}, {2}, ({3}), {4}'.format(user['primaryEmail'],user['name']['givenName'],user['name']['familyName'],user['name']['fullName'],user['orgUnitPath']))
+					#else:
+					#	continue
+			if results.get('nextPageToken') == None:
+				#print (errors)
+				return False
+			else:
+				cont = (results.get('nextPageToken'))
+				
+		except Exception, e:
+			#(errors + 1)
+			pass
 	
 # TODO Write loop for all pageTokens and then stop when you run out
 
-	results2 = service.users().list(domain = 'avx.com', maxResults = 500, orderBy = 'email', pageToken = cont).execute()
+	# results2 = service.users().list(domain = 'avx.com', maxResults = 500, orderBy = 'email', pageToken = cont).execute()
 	
-	users2 = results2.get('users', [])
+	# users2 = results2.get('users', [])
 	
-	if not users2:
-		print ('No more')
-	else:
-		print('Users:')
-		try:
-			for user in users2:
-				#if user['creationTime'] >= week_ago:
-				print (u'{0}, {1}, {2}, {3}, {4}'.format(user['primaryEmail'],user['name']['givenName'],user['name']['familyName'],user['name']['fullName'],user['orgUnitPath']))
-		except Exception, e:
-			pass
-		print (len(users2))
+	# if not users2:
+		# print ('No more')
+	# else:
+		# print('Users:')
+		# try:
+			# for user in users2:
+				# #if user['creationTime'] >= week_ago:
+				# print (u'{0}, {1}, {2}, {3}, {4}'.format(user['primaryEmail'],user['name']['givenName'],user['name']['familyName'],user['name']['fullName'],user['orgUnitPath']))
+		# except Exception, e:
+			# pass
+		# print (len(users2))
 				
 				#results.get('users', pageToken = users['nextPageToken'])
 
